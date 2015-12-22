30c98f5b283929d127238526a671cf8bb8aa51f5
Filipe Manuel Goncalves de Oliveira <filipemgoliveira94@gmail.com>
2015-12-22 12:00:04 +0000

    Tela Login

--- src/dataacess/ListaDAO.java
+++ src/dataacess/ListaDAO.java
@@ -8,6 +8,7 @@ package dataacess;
 import business.Lista;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Collection;
@@ -19,9 +20,9 @@ import java.util.Set;
  * @author Filipe Oliveira
  */
 public class ListaDAO implements Map<String, Lista> {
-
+    
     private Connection conn;
-
+    
     public ListaDAO() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -30,74 +31,101 @@ public class ListaDAO implements Map<String, Lista> {
             l.printStackTrace();
         }
     }
-
+    
     public Lista put(String tipoL, Lista value) {
         
         try {
             Lista l = null;
             Statement stm = conn.createStatement();
-            String sql = "INSERT INTO lista (nomeLista, tipo, Eleicao_nEleicao) VALUES (\"" + value.getNomeLista() + "\",\"" + value.getTipoLista() + "\",\"" + value.getNumEleicaoParticipa() + "\");";
+            String sql = "INSERT INTO lista (nomeLista, tipo, validacao, Eleicao_nEleicao) VALUES (\"" + value.getNomeLista() + "\",\"" + value.getTipoLista() + "\", \"" +value.getValidacao()+ "\",\"" + value.getNumEleicaoParticipa() + "\");";
             int i = stm.executeUpdate(sql);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return new Lista(value.getIdLista(),value.getTipoLista(),value.getNomeLista(),value.getNumEleicaoParticipa());
-
+        return new Lista(value.getIdLista(), value.getTipoLista(), value.getNomeLista(),value.getValidacao(), value.getNumEleicaoParticipa());
+        
     }
-
+    
     @Override
     public int size() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
     @Override
     public boolean isEmpty() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
     @Override
     public boolean containsKey(Object key) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
     @Override
     public boolean containsValue(Object value) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
+    public int getIdLista(String nome) {
+        int i = 0;
+        try {
+            Statement stm = conn.createStatement();
+            String sql = "SELECT * FROM lista WHERE nomeLista = '" + nome + "';";
+            ResultSet rs = stm.executeQuery(sql);
+            if (rs.next()) {
+            i = rs.getInt(1);
+            }
+        }
+        catch(Exception e){
+            throw new NullPointerException(e.getMessage());
+        }
+            return i;
+    }
+    
     @Override
     public Lista get(Object key) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Lista le = null;
+        try {
+            Statement stm = conn.createStatement();
+            String sql = "SELECT * FROM lista WHERE nome = '" + (String) key + "';";
+            ResultSet rs = stm.executeQuery(sql);
+            if (rs.next()) {
+            le = new Lista(rs.getInt(1), rs.getString(2), rs.getString(3),rs.getString(4), rs.getInt(5));
+            }
+        } catch (Exception e) {
+            throw new NullPointerException(e.getMessage());
+        }
+        return le;
     }
-
+    
     @Override
     public Lista remove(Object key) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
     @Override
     public void putAll(Map<? extends String, ? extends Lista> m) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
     @Override
     public void clear() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
     @Override
     public Set<String> keySet() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
     @Override
     public Collection<Lista> values() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
     @Override
     public Set<Entry<String, Lista>> entrySet() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
 }
